e349b36591d5b5b4aae1baf9063f9d358e10c249
Swagat003 <109663081+Swagat003@users.noreply.github.com>
2024-01-06 00:13:13 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -155,6 +155,10 @@ function playSong(id) {
     play.innerHTML = `<svg viewBox="0 0 24 24" ><path d="M5.7 3a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7H5.7zm10 0a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7h-2.6z" fill="white"></path></svg>`
     playing = true;
     audio.addEventListener("timeupdate", progress);
+    audio.addEventListener("ended", () => {
+        play.innerHTML = `<svg viewBox="0 0 24 24"><path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z" fill="white"></path></svg>`
+        playing = false;
+    })
 }
 
 player.style.backgroundColor = getHslColor();
