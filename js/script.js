44b92eac0ce827ece66c4bc588f43020284fe27d
Swagat003 <109663081+Swagat003@users.noreply.github.com>
2024-01-17 21:18:43 +0530

    PWA fix

--- js/script.js
+++ js/script.js
@@ -296,5 +296,12 @@ full_bar_container.addEventListener("pointerdown", (e) => {
     }, { once: true });
 });
 
-
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker
+            .register("/serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err))
+    })
+}
 
---
+++ serviceWorker.js
@@ -0,0 +1,46 @@
+const spotifyClone = "spotify-clone";
+const assets = [
+    "/",
+    "/index.html",
+    "/css/style.css",
+    "/css/utility.css",
+    "/js/script.js",
+    "/images/song-img1.jpg",
+    "/images/song-img2.jpg",
+    "/images/song-img3.jpg",
+    "/images/song-img4.jpg",
+    "/images/song-img5.jpg",
+    "/images/song-img6.jpg",
+    "/images/song-img7.jpg",
+    "/images/song-img8.jpg",
+    "/images/song-img9.jpg",
+    "/images/song-img10.jpg",
+    "/images/song-img11.jpg",
+    "/songs/song1.mp3",
+    "/songs/song2.mp3",
+    "/songs/song3.mp3",
+    "/songs/song4.mp3",
+    "/songs/song5.mp3",
+    "/songs/song6.mp3",
+    "/songs/song7.mp3",
+    "/songs/song8.mp3",
+    "/songs/song9.mp3",
+    "/songs/song10.mp3",
+    "/songs/song11.mp3"
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(spotifyClone).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
